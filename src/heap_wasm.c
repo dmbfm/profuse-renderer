da305151ecc3bb1a954a6d56c3e401b7ccf603fb
Daniel Fortes <daniel.mbfm@gmail.com>
2020-12-22 16:20:14 -0300

    Last alloc day afternoon

--- src/heap_wasm.c
+++ src/heap_wasm.c
@@ -131,10 +131,9 @@ typedef struct HeapWasmFreeListAllocatorState
     usize default_alignment;
 } HeapWasmFreeListAllocatorState;
 
+extern void *memcpy(void *destination, const void *source, size_t num);
 
-extern void * memcpy ( void * destination, const void * source, size_t num );
-
-static ErrorCode wasm_free_list_allocator_init(HeapWasmFreeListAllocatorState *allocator, usize default_alignment)
+local ErrorCode wasm_free_list_allocator_init(HeapWasmFreeListAllocatorState *allocator, usize default_alignment)
 {
     heap_wasm_memory_init(&allocator->heap);
 
@@ -148,16 +147,17 @@ static ErrorCode wasm_free_list_allocator_init(HeapWasmFreeListAllocatorState *a
     allocator->head.prev = 0;
 
     // Align first block so that the header fits before the user pointer
-    Result(uptr) r_first = mem_align_pointer_forward(allocator->heap.start + sizeof(BlockHeader), allocator->default_alignment);
+    Result(uptr) r_first =
+        mem_align_pointer_forward(allocator->heap.start + sizeof(BlockHeader), allocator->default_alignment);
 
-    // Failed to align!
+    // Failed to align?
     result_raise_error_code(r_first);
 
     // This is the first user pointer
     uptr first = r_first.value;
 
     // If we can't fit the footer with something remaining, we're out of memory
-    if ((allocator->heap.end - sizeof(BlockFooter)) <=  first) {
+    if ((allocator->heap.end - sizeof(BlockFooter)) <= first) {
         return ERR_OUT_OF_MEMORY;
     }
 
@@ -171,9 +171,9 @@ static ErrorCode wasm_free_list_allocator_init(HeapWasmFreeListAllocatorState *a
     BlockFooter footer = { .flag = BLOCK_FREE, .size = block_size };
 
     // Calculate where the header and footer should be stored at
-    BlockHeader *header_ptr = (BlockHeader *) (first - sizeof(BlockHeader)); 
-    BlockFooter *footer_ptr = (BlockFooter *) (allocator->heap.end - sizeof(BlockFooter));
-    
+    BlockHeader *header_ptr = (BlockHeader *)(first - sizeof(BlockHeader));
+    BlockFooter *footer_ptr = (BlockFooter *)(allocator->heap.end - sizeof(BlockFooter));
+
     // Copy the header and footer data
     *header_ptr = header;
     *footer_ptr = footer;
@@ -182,35 +182,102 @@ static ErrorCode wasm_free_list_allocator_init(HeapWasmFreeListAllocatorState *a
     allocator->head.next = header_ptr;
     allocator->head.prev = header_ptr;
 
+    // Initialization was successfull :)
+    allocator->initialized = true;
+
     return ERR_OK;
 }
 
-void wasm_free_list_allocator_remove_block(BlockHeader *h)
+local void wasm_free_list_allocator_remove_block(BlockHeader *h)
 {
     h->next->prev = h->prev;
     h->prev->next = h->next;
 }
 
-void wasm_free_list_allocator_prepend_block(HeapWasmFreeListAllocatorState *allocator, BlockHeader *h)
+local void wasm_free_list_allocator_prepend_block(HeapWasmFreeListAllocatorState *allocator, BlockHeader *h)
 {
-    h->next = allocator->head.next;
-    h->prev = &allocator->head;
+    h->next       = allocator->head.next;
+    h->prev       = &allocator->head;
     h->next->prev = h;
     h->prev->next = h;
 }
 
-void wasm_free_list_allocator_alloc(HeapWasmFreeListAllocatorState *allocator, usize amount)
+local Result(uptr) wasm_free_list_allocator_alloc(HeapWasmFreeListAllocatorState *allocator, usize amount)
 {
-    if (!allocator->initialized) wasm_free_list_allocator_init(allocator);
+    // If the allocator was not initialized, initialize it with the default alignment
+    if (!allocator->initialized)
+        wasm_free_list_allocator_init(allocator, HEAP_WASM_MAX_ALIGN);
 
-    // Go through each free block in the list...
+    // Total amount needed to create a new aligned block = AlignForward(amount + H + F)
+    usize amount_needed_for_new_block = result_unwrap(mem_align_pointer_forward(
+        (uptr)(amount + sizeof(BlockHeader) + sizeof(BlockFooter)), allocator->default_alignment));
+
+    // Traverse the free block list
     BlockHeader *current = &allocator->head;
-    while((current = current->next) != &allocator->head) {
-        // Found a free block
-        if (current->flag == BLOCK_FREE) {
+    while ((current = current->next) != &allocator->head) {
 
+        // If the current block has enough capacity...
+        if (current->size >= amount) {
+
+            // Get the pointer to the allocated region
+            uptr user_pointer = (uptr) current + sizeof(BlockHeader);
+
+            // Double-check alignment
+            assert(mem_is_aligned(user_pointer, allocator->default_alignment));
+
+            if (current->size > amount_needed_for_new_block) {
+                // In this case, there is enough space to add a new free block after the allocated one
+
+                // The effective amount that will be allocated, more ore equal than "amount"
+                usize allocated_amount = amount_needed_for_new_block - sizeof(BlockFooter) - sizeof(BlockHeader);
+                usize initial_size = current->size;
+
+                // Set the allocated amount on the header, and remove the block from the free list
+                current->size = allocated_amount;
+                current->flag = BLOCK_RESERVED;
+                wasm_free_list_allocator_remove_block(current);
+
+                // Set the footer of the current block 
+                uptr current_footer = (uptr) current + sizeof(BlockHeader) + current->size;
+                ((BlockFooter *)current_footer)->flag = BLOCK_RESERVED;
+                ((BlockFooter *)current_footer)->size = current->size;
+
+                // Set the header of the created block
+                usize next_block_size = initial_size - sizeof(BlockFooter) - sizeof(BlockHeader) - allocated_amount;
+                uptr next_header = (uptr) current_footer + sizeof(BlockFooter);
+                ((BlockHeader *) next_header)->flag = BLOCK_FREE;
+                ((BlockHeader *) next_header)->size = next_block_size;
+
+                // Set the footer of the created block
+                uptr next_footer = (uptr)current + initial_size;
+                ((BlockFooter *) next_footer)->flag = BLOCK_FREE;
+                ((BlockFooter *) next_footer)->size = next_block_size;
+
+                // Add the created block to the free list
+                wasm_free_list_allocator_prepend_block(allocator, (BlockHeader *) next_header);
+
+            } else {
+                // Allocate the whole block 
+
+                // Mark header as reserved
+                current->flag = BLOCK_RESERVED;
+
+                // Mark footer as reserved
+                BlockFooter *footer = (BlockFooter *)current + sizeof(BlockHeader) + current->size;
+                footer->flag = BLOCK_RESERVED;
+                footer->size = current->size;
+
+                // Remove block from free list
+                wasm_free_list_allocator_remove_block(current);
+            }
+
+            // If everything worked, return the allocated region's pointer
+            return result_ok(uptr, user_pointer);
         }
     }
+
+    // If no block with the required size was found, we're out of memory
+    return result_error(uptr, ERR_OUT_OF_MEMORY);
 }
 
 //#ifdef __RUN_TESTS
@@ -231,7 +298,7 @@ typedef struct HeapWasmMockMemory
     usize num_pages;
 } HeapWasmMockMemory;
 
-static HeapWasmMockMemory __mock_memory;
+local HeapWasmMockMemory __mock_memory;
 
 u32 wasm_get_memory_size()
 {
